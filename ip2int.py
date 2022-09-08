f8a74abc1895935826a35cda8366546c9720d914
Oleganin <oleganin@gmail.com>
2022-09-08 11:18:47 +0300

    chenge ip2int

--- ip2int.py
+++ ip2int.py
@@ -1,10 +1,9 @@
 def ip2int(s_ip):
+    tmf = lambda x: '0'+x.split('x')[-1] if len(x)<4 else x.split('x')[-1]
     tmp = map( hex, map(int, s_ip.split('.')))
-    return int(''.join(
-        map(
-            lambda x: '0'+x.split('x')[-1] if len(x)<4 else x.split('x')[-1], tmp
-            )
-        ),16)
+    return int(
+        ''.join(map(tmf , tmp)),
+        16)
 
 if __name__ == '__main__':
     print(ip2int('128.32.10.1'))
