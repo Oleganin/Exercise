814cc06393619e6359188fd62df2bcde8626c16b
Oleganin <oleganin@gmail.com>
2022-10-05 16:13:40 +0300

    Chenged url, Done

--- url.py
+++ url.py
@@ -7,30 +7,34 @@ def get_scheme(data):
     return data.scheme
 
 def set_scheme(data, scheme):
-    new_url = to_string(data._replace(scheme=scheme))
-    return make(new_url)
+    return data._replace(scheme=scheme)
 
 def get_host(data):
     return data.hostname
 
 def set_host(data, host):
-    new_url = to_string(data._replace(netloc=host))
-    return make(new_url)
+    return data._replace(netloc=host)
 
 def get_path(data):
     return data.path
 
 def set_path(data, path):
-    new_url = to_string(data._replace(path=path))
-    return make(new_url)
+    return data._replace(path=path)
 
 def get_query_param(data, param_name, default = None):
     if not data.query:
         return default
-    return urllib.parse.parse_qs(data.query)[param_name]
+    return urllib.parse.parse_qs(data.query)[param_name][-1]
 
 def set_query_param(data, key, value):
-    pass
+    new_query_dict = urllib.parse.parse_qs(data.query)
+    if value:
+        new_query_dict.update({key: value})
+    else:
+        del new_query_dict[key]
+    new_query = urllib.parse.urlencode(new_query_dict, doseq=True)
+    return data._replace(query=new_query)
+    
 
 def to_string(data):
     return urllib.parse.urlunparse(data)
@@ -39,10 +43,12 @@ if __name__ == '__main__':
     u = make('https://hexlet.io/community?q=low')
     u = set_scheme(u, 'http')
     print(to_string(u))
+    u = set_path(u, '/404')
+    print(to_string(u))
     print(get_query_param(u, 'q'))
-    '''u = set_query_param(u, 'page', 5)
+    u = set_query_param(u, 'page', 5)
     print(to_string(u))
     u = set_query_param(u, 'q', 'high')
     print(to_string(u))
     u = set_query_param(u, 'q', None)
-    print(to_string(u))'''
+    print(to_string(u))
