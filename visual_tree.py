1c09b3b04db45c31c0d018a8c1e8cf8a6c781d20
Oleganin <oleganin@gmail.com>
2022-10-12 16:08:10 +0300

    Added visual_tree

---
+++ visual_tree.py
@@ -0,0 +1,16 @@
+from hexlet import fs
+
+def visual_tree(tree, deep = 0):
+    logo = f'({fs.get_name(tree)})'
+    if fs.get_meta(tree):
+        logo += f'-{fs.get_meta(tree)}'
+    print(logo)
+    for node in fs.get_children(tree):
+        if fs.is_file(node):
+            print('  '*deep+'|_', end='')
+            print(fs.get_name(node))
+        if fs.is_directory(node):
+            print('  '*deep+'|_', end='')
+            deep+=1
+            visual_tree(node, deep)
+            deep-=1
