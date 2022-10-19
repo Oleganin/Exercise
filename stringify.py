51be438b9a0a377ef3f268d47cb8cd2a7340c6c8
Oleganin <oleganin@gmail.com>
2022-10-19 10:10:53 +0300

    Corected stringify

--- stringify.py
+++ stringify.py
@@ -1,18 +1,28 @@
 def stringify(value, replacer = ' ', spaces_count = 1):
-    result = ''
-    if isinstance(value, dict):
-        result +='{'
-        start = f'\n{replacer*(spaces_count)}'
-        spaces_count+=1
-        for key in value:
-            result += f'{start}{key}: {stringify(value[key],replacer,spaces_count)}'
-        spaces_count-=1
-        result += f'\n{replacer*(spaces_count)}'
-        result += '}'
-    else:
-        result += str(value)
-    return result
+    def create_string(value, lvl=1):
+        res = ''
+        if isinstance(value, dict):
+            res +='{'
+            start = f'\n{replacer*(spaces_count)*lvl}'
+            lvl+=1
+            for key in value:
+                res += f'{start}{key}: {create_string(value[key],lvl)}'
+            lvl-=1
+            res += f'\n{replacer*(spaces_count)*(lvl-1)}'
+            res += '}'
+        else:
+            res += str(value)
+        return res
+    result = create_string(value)
+    print(result)
 
-data = {'hello':'worls', 'is':True, 'nested':{'count':5}}
-print(stringify(data))
-print(stringify(data, '|-', 2))
+if  __name__ == '__main__':
+    stringify('hello')
+    stringify(True)
+    stringify(5)
+    data = {'hello':'world', 'is':True, 'nested':{'count':5}}
+    stringify(data)
+    stringify(data, '|-', 2)
+    data2 = {'hello':'world', 'is':True, 'nested':{'count':5}, 'list':[1,2,3]}
+    stringify(data2)
+    stringify(data2, '|_', 3)
