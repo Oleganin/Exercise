ce397f5cbaf090ddc4159e9199240dafb9ed4eea
Oleganin <oleganin@gmail.com>
2022-10-27 14:22:40 +0300

    Chenge re

--- re.py
+++ re.py
@@ -1,34 +1,32 @@
 import re
-'''
-text = 'java \ python ruby1.9 javascript c#'
-patern = r'ruby1\..'
-need = 'ruby1. and simbol'
-result = re.findall(patern, text)
-print(f'need find\n"{need}"\nin\n"{text}"\nThe result: "{result}"')
-print('-'*40)
 
-text = 'java \ python ruby1.9 11/& javascript c#, 54/f, 55/F'
-patern = r'\d\d/[^a-z]'
-need = 'two digit "/" and any simbol without "a-z"'
-result = re.findall(patern, text)
-print(f'need find\n"{need}"\nin\n"{text}"\nThe result: "{result}"')
-print('-'*40)
-'''
-
-def find(patern, text, commit=''):
-    res = re.findall(patern, text)
+def find(patern, args, commit=''):
     if commit:
         print(f'Need find\n"{commit}"')
-    print(f'In\n"{text}"\nThe result: "{res}"')
+    for text in args:
+        res = re.findall(patern, text)
+        print(f'In "{text}". ', end='') 
+        if res:
+              print(f'Finded: "{res}"')
+        else:
+            print('Nothing find')
     print('-'*40)
 
 
-find(r'ruby1\..', 'java \ python ruby1.9 javascript c#')
 find(r'ruby1\..',
-     'java \ python ruby1.9 javascript c#',
-     'ruby1. and simbol'
+     ['java \ python ruby1.9 javascript c#'],
+     'ruby1. and some simbol'
      )
-find(r'\d\d/[^a-z]', 'java \ python ruby1.9 11/& javascript c#, 54/f, 55/F')
 find(r'\d\d/[^a-z]',
-     'java \ python ruby1.9 11/& javascript c#, 54/f, 55/F',
-     'two digit "/" and any simbol without "a-z"')
+     ['java \ python ruby1.9 11/& javascript c#, 54/f, 55/F'],
+     'two digit "/" and something simbol without "a-z"')
+find(r'^support@hexlet\.io$',
+     ['something here support@hexlet.io',
+      'support@hexlet.io something here',
+      'support@hexlet.io'],
+     'Find only \'support@hexlet.io\''
+     )
+find(r'(o|t)(n|w|hre)(e|o)',
+     ['one','two','three', 'four','five',
+      'one, two', 'one two three']
+     )
