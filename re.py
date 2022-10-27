1738d9d18b3ba55f0abda5298a631c9367374c0c
Oleganin <oleganin@gmail.com>
2022-10-27 11:01:55 +0300

    Chenge re

--- re.py
+++ re.py
@@ -1,5 +1,5 @@
 import re
-
+'''
 text = 'java \ python ruby1.9 javascript c#'
 patern = r'ruby1\..'
 need = 'ruby1. and simbol'
@@ -13,4 +13,22 @@ need = 'two digit "/" and any simbol without "a-z"'
 result = re.findall(patern, text)
 print(f'need find\n"{need}"\nin\n"{text}"\nThe result: "{result}"')
 print('-'*40)
+'''
 
+def find(patern, text, commit=''):
+    res = re.findall(patern, text)
+    if commit:
+        print(f'Need find\n"{commit}"')
+    print(f'In\n"{text}"\nThe result: "{res}"')
+    print('-'*40)
+
+
+find(r'ruby1\..', 'java \ python ruby1.9 javascript c#')
+find(r'ruby1\..',
+     'java \ python ruby1.9 javascript c#',
+     'ruby1. and simbol'
+     )
+find(r'\d\d/[^a-z]', 'java \ python ruby1.9 11/& javascript c#, 54/f, 55/F')
+find(r'\d\d/[^a-z]',
+     'java \ python ruby1.9 11/& javascript c#, 54/f, 55/F',
+     'two digit "/" and any simbol without "a-z"')
