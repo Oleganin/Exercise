30d83693cf29719b5c9b6d4d68f46a5e7a20008f
Kukushkin Oleh Eduardovich <Oleganin@gmail.com>
2022-10-31 20:19:06 +0200

    Chenge re

--- re.py
+++ re.py
@@ -1,18 +1,24 @@
 import re
 
 def find(patern, args, commit=''):
+    res = re.compile(patern)
     if commit:
-        print(f'Need find\n"{commit}"')
+        print('Need find'.center(80))
+        print(f'"{commit}"'.center(80)+'\n')
     for text in args:
-        res = re.findall(patern, text)
-        print(f'In "{text}". ', end='') 
-        if res:
-              print(f'Finded: "{res}"')
+        tmp = transform(res.finditer(text))
+        print(f'In "{text}". ') 
+        if tmp:
+              print(f'\tFinded: "{tmp}"')
         else:
-            print('Nothing find')
-    print('-'*40)
+            print('\tNothing find')
+    print('-'*80)
 
-
+def transform(args):
+    elements = map(lambda x: x.group(), args)
+    res = ', '.join(elements)
+    return res
+    
 find(r'ruby1\..',
      ['java \ python ruby1.9 javascript c#'],
      'ruby1. and some simbol'
@@ -28,5 +34,28 @@ find(r'^support@hexlet\.io$',
      )
 find(r'(o|t)(n|w|hre)(e|o)',
      ['one','two','three', 'four','five',
-      'one, two', 'one two three']
+      'one, two', 'one two three'],
+     'Regular expression for word: "one", "two", "three"'
+     )
+find(r'\w+@.{3,}\..{2,5}$',
+     ['info@helxet.io','Oleganin@gmail.com','Olegik_dnepr@mail.ru',
+      'Olegik_dnepr@mail.remember','Olegik_dnepr@m.ru',
+      'Olegik_dnepr@mail.r',
+      'Hello, my email is : info@helxet.io, Oleganin@gmail.com'],
+     'Regular expression find email'
+     )
+find(r'\(.+\)',
+     ['()','decart (0,2),(4,4)','it impossible (never)',
+      '(_)(-)(.)','Olegik_dnepr@m.ru'],
+     'Find "(" least one simbol ")"'
+     )
+find(r'(?P<group1>[a-z]{3})\:(?P=group1)',
+     ['aaa:fff','is bad:bad good,(4,4)','aa:. aaa:.dfsg gih:fit, boy:boy'
+      ],
+     'Find three char "a-z" ":" and again three char'
+     )
+find(r'80\:(?=[^a-z]+)',
+     ['80:8080, 80:!@#$','80:','80','80:d123f',
+      ],
+     'Find 80: any symbol without a-z'
      )
